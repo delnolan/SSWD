0715de2b22ae45650837ec3d830b21251efda387
Derek Nolan <delnolan@gmail.com>
2018-08-05 00:59:56 +0100

    Comments added to Users_Model.php

--- application/models/Users_Model.php
+++ application/models/Users_Model.php
@@ -1,27 +1,30 @@
 <?php 
 class Users_Model extends CI_Model {
 	
-	function get_all_users(){
+	function get_all_users(){ //This function returns a list of users returning user_id and username
 		
-		$users = $this->db->get('users')->result();  
-		$users = json_encode($users);
-		return $users;
+		$users = $this->db->get('users')->result();  //Codeigniter function for mySql Select
+		$users = json_encode($users); //Convert to json for output
+		return $users; //output to controller
 	
 	}	
 	
-	function getUsersBySinId($sinId){
+	function getUsersBySinId($sinId){ //This function returns all users (user_id and username) who have committed this sin (sin_id)
 		
+		//This gives access to user ids by accessing sins_meta by sin id
 		$userIds = $this->db->get_where('sins_meta', array('sin_id' => $sinId))->result();
-		//$userIds = $this->db->get_where('users', array('sin_id' => $sinId))->row()->sin_id;
 		
+		//An empty array to which we can add our results
 		$users = array();
+		
+		//Below loops through the results to find each user who committed the sin by user id
 		foreach($userIds as $user){
 			$result = $this->db->get_where('users', array('user_id' => $user->user_id))->row();
-			array_push($users,$result);
+			array_push($users,$result); //push to our array for each iteration
 		}
 
-		$users = json_encode($users);
-		return $users;
+		$users = json_encode($users); //convert to json for output
+		return $users; //output to controller
 		
 	}
 

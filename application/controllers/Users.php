3a418820f1b9e6ace072f09db00ffc4c75d142aa
Derek Nolan <delnolan@gmail.com>
2018-08-05 01:17:38 +0100

    Comments added to Users.php

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -3,18 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Users extends CI_Controller {
 
-	public function index()
+	public function index() //Loads on index.php/Users
 	{
-		$this->load->model('Users_Model');
-		$sinId = $this->input->get('sinid');
+		$this->load->model('Users_Model'); //Gives us access to User_Model.php classes and functions
+		$sinId = $this->input->get('sinid'); //Gets the sin id if it is set using a $_GET request
 		
-		if(isset($sinId)){
-			$users = $this->Users_Model->getUsersBySinId($sinId);
-			print_r($users);
-			die();
+		if(isset($sinId)){ //If sin id is set using a $_GET request then execute this block
+			$users = $this->Users_Model->getUsersBySinId($sinId); //Get all users by sin id
+			print_r($users); //Output
+			die(); //Exit
 		}
 		
-		$users = $this->Users_Model->get_all_users();
-		print_r($users);
+		$users = $this->Users_Model->get_all_users(); //Get all users
+		print_r($users); //Output
 	}
 }
